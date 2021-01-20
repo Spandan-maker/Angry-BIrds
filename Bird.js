9b44bb4821a8eb3b8b2b11052b0752e833ae556d
Spandan <74659727+Spandan-maker@users.noreply.github.com>
2021-01-20 16:53:51 +0530

    Update Bird.js

--- Bird.js
+++ Bird.js
@@ -3,12 +3,10 @@ class Bird extends BaseClass {
     super(x,y,50,50);
     this.image = loadImage("sprites/bird.png");
     this.smokeImage = loadImage("sprites/smoke.png");
-    this.trajectory =[];
+    this.trajectory = [];
   }
 
   display() {
-    //this.body.position.x = mouseX;
-    //this.body.position.y = mouseY;
 
     super.display();
 
@@ -16,9 +14,10 @@ class Bird extends BaseClass {
       var position = [this.body.position.x, this.body.position.y];
       this.trajectory.push(position);
     }
-   
 
     for(var i=0; i<this.trajectory.length; i++){
+      console.log("Trajectory value: "+this.trajectory[i][0]+","+this.trajectory[i][1]);
+
       image(this.smokeImage, this.trajectory[i][0], this.trajectory[i][1]);
     }
   }
