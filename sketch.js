1236efe3a6332f287c774fa00abbca7d45b5197b
Spandan <74659727+Spandan-maker@users.noreply.github.com>
2021-01-23 18:59:30 +0530

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -102,19 +102,19 @@ function keyPressed(){
 }
 
 async function getBackgroundImg(){
-    var response = await fetch("http://worldtimeapi.org/api/timezone/Asia/Kolkata");
-    var responseJSON = await response.json();
+    //var response = await fetch("http://worldtimeapi.org/api/timezone/Asia/Kolkata");
+    //var responseJSON = await response.json();
 
-    var datetime = responseJSON.datetime;
-    var hour = datetime.slice(11,13);
+    //var datetime = responseJSON.datetime;
+    //var hour = datetime.slice(11,13);
     
-    if(hour>=06 && hour<=19){
+    //if(hour>=06 && hour<=19){
         bg = "sprites/bg1.png";
-    }
-    else{
-        bg = "sprites/bg2.jpg";
-    }
+    //}
+    //else{
+        //bg = "sprites/bg2.jpg";
+    //}
 
     backgroundImg = loadImage(bg);
-    console.log(backgroundImg);
+    //console.log(backgroundImg);
 }
